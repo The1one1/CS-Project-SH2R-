77ebcd1e7c6282b737c17ae98e6fe465504b367a
Rahul Sharma <76915617+The1one1@users.noreply.github.com>
2021-07-11 18:18:54 +0530

    Updated files

---
+++ coffee.hpp
@@ -0,0 +1,12 @@
+#ifndef coffee_hpp
+#define coffee_hpp
+
+namespace example
+{
+    class coffee_machine
+    {
+        public:
+        void coffee_choice();
+    };
+}
+#endif
--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
-//#include"money.h"
-#include"final_ingridient.h"
+#include"coffee.hpp"
 #include"feedback.h"
 #include"ingridient.h"
+#include"final_ingridient.h"
+
 using namespace std;
 
-// using example::calmainculation;
 using example::feedback;
 using example::final_detail;
-using example::ingridient;
-//using example::money_input;
+using example::coffee_machine;
 using example::newfeedback;
 using example::printfeedback;
 using example::value1;
@@ -17,42 +16,8 @@ using example::value2;
 using example::value3;
 using example::value4;
 
-//function to select coffee
-void coffee_choice()
-{ 
-    string choice;
-    cout<<"What would you like(default C):(L,E,C)::\n"; 
-    cout<<"-----------------------    -----------------------    -----------------------\n";
-    cout<<"|        Latte:       |";
-    cout<<"    |       Esprsso:      |";
-    cout<<"    |      Cappucino:     |\n";
-    cout<<"-----------------------    -----------------------    -----------------------\n\n";
-    cin>>choice;
-    cout<<"\n";
-    
-    if(choice=="Latte"||'l'||'L')
-    {
-        ingridient latte1(200, 150, 24, 20);   //class object
-//        latte1.latt();
-		latte1.coffee_cost();
-		latte1.remaning_amount(); 
-    }
-    else if(choice=="Espresso"||'e'||'E')
-    {
-        ingridient espressor1(50, 0, 18, 30);  //class object
-//        espressor1.latt();
-		espressor1.coffee_cost();
-		espressor1.remaning_amount();
-    }
-    else
-    {
-        ingridient cappuccino1(250, 50, 24, 50);   //class object
-//        cappuccino1.latt();
-		cappuccino1.coffee_cost();
-		cappuccino1.remaning_amount();
-    }
-}
 final_detail g;     //class object
+coffee_machine c;   //class object
 
 //Driver program
 int main()
@@ -70,7 +35,7 @@ int main()
         cin>>x;
         cout<<"\n";
         if(x==1)
-            coffee_choice();
+            c.coffee_choice();
         else if(x==2)
             newfeedback();
         else if(x==3)
